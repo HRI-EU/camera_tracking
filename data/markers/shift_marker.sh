b8069ae97b20cd1d8191e599a810e6fb8b04a54e
Stephan Hasler <stephan.hasler@honda-ri.de>
2023-11-27 14:16:30 +0100

    remvoved obsolete spaces

--- data/markers/shift_marker.sh
+++ data/markers/shift_marker.sh
@@ -16,10 +16,9 @@
 start=11
 end=22
 offset=12
-old_file=aruco_${start}_to_${end}_4cm.svg 
+old_file=aruco_${start}_to_${end}_4cm.svg
 new_file=aruco_$((start+offset))_to_$((end+offset))_4cm.svg
 echo "${old_file} -> ${new_file}"
 cp ${old_file} ${new_file}
 for (( a=start; a<=end; a++ )); do b=$((a+offset)); echo "marker_$a -> marker_$b"; sed -i "s:marker_$a:marker_$b:g" ${new_file}; done
 for (( a=start; a<=end; a++ )); do b=$((a+offset)); echo ">$a< -> >$b<"; sed -i "s:>$a<:>$b<:g" ${new_file}; done
-
