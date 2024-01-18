951e06346213bfb60d37dbfd5615886eb4ede787
Stephan Hasler <stephan.hasler@honda-ri.de>
2024-01-18 15:11:47 +0100

    fixed bug

--- data/markers/replace_marker_ids.sh
+++ data/markers/replace_marker_ids.sh
@@ -22,8 +22,8 @@ fi
 
 have_marker_ids=(11 12 13 14 15 16 17 18 19 20 21 22)
 
-# The dummy is used to prevent multiple sequential replacements of a pattern.
-dummy="blablu"
+# The dummy is used to prevent multiple sequential replacements of a pattern. It is later removed again.
+dummy="alreadydone"
 
 replacement=""
 filename=""
@@ -32,14 +32,11 @@ for (( i=0; i<12; i++ )); do
     have_id=${have_marker_ids[i]}
     want_id=${want_marker_ids[i]}
     replacement+="s:marker\_${have_id}:marker_${dummy}${want_id}:g;"
-    replacement+="s:>${have_id}<:>${want_id}${dummy}<:g; "
+    replacement+="s:>${have_id}<:>${want_id}${dummy}<:g;"
     filename+="_${want_id}"
 done
-
-
-echo "$replacement"
 
 fullname=aruco"${filename}"_4cm.svg
 
 sed "${replacement}" aruco_11_to_22_4cm.svg > "${fullname}"
-sed -i "s:${stopper}::g" "${fullname}"
+sed -i "s:${dummy}::g" "${fullname}"
